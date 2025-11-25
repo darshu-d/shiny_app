c583a188e263d839fd59dcbb0f0278918c580217
Sivakamakshi Muthu Kumarasamy <sivaopt@gmail.com>
2025-11-25 11:18:58 +0000

    New side panels created

--- app.R
+++ app.R
@@ -8,44 +8,22 @@
 #
 
 library(shiny)
-
-# Define UI for application that draws a histogram
+dig_dataset <- read.csv("C:/Users/sivak/Downloads/DIG-1.csv")
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Digitalis Investigation Group"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "TRTMT", label = "Select Treatment type", choices = c("Digoxin", "Placebo"), multiple = FALSE),
+      selectInput(inputId = "Sex", label = "Select patient sex", choices = c("Male", "Female"), multiple = FALSE)
+    ),
+    mainPanel(
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
